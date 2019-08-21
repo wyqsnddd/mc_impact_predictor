a44ee4139fbf263f41ba66770f077884e8cd9982
yuquan <wyqsnddd@gmail.com>
2019-08-21 10:48:59 +0200

    Change namespace from 'mi_impact' to 'mc_impact'

---
+++ include/mi_utils.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <iostream>
+#include <map>
+
+namespace mc_impact
+{
+
+struct qpEstimatorParameter
+{
+  double Qweight = 20;
+  // std::vector<std::string> impactBodyNames={"r_wrist"};
+  std::map<std::string, Eigen::Vector3d> impactNameAndNormals;
+  double impactDuration = 0.005;
+  double timeStep = 0.005;
+  double coeFrictionDeduction = 0.2;
+  double coeRes = 0.8;
+  int dim = 3;
+  bool useLagrangeMultiplier = false;
+  bool useJsd = true;
+  bool useOsd = true;
+};
+
+struct endEffector
+{
+  // size_t startingNumber; // Starting number in the vector
+  int uniqueIndex;
+  Eigen::Vector3d eeVJump;
+  Eigen::Vector3d estimatedImpulse;
+  Eigen::Vector3d estimatedAverageImpulsiveForce;
+  Eigen::Vector3d checkForce;
+  Eigen::MatrixXd jacobianDeltaF;
+};
+
+} // namespace mi_impact
